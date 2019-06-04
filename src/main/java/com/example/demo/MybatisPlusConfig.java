e9cbadbfd302a8d93760e58895e20e19ad908d28
admin <tangwei@anji-tec.com>
2019-06-04 09:44:46 +0800

    可以部署（大坑是没有配置DataSource）

--- src/main/java/com/example/demo/MybatisPlusConfig.java
+++ src/main/java/com/example/demo/MybatisPlusConfig.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
+import java.beans.PropertyVetoException;
+
+import javax.sql.DataSource;
+
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 
 //Spring boot方式
@@ -15,6 +21,31 @@ import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 @Configuration
 @MapperScan(value = {"com.example.demo.dao.intf", "com.**.mapper"})
 public class MybatisPlusConfig {
+	
+	
+	@Value("${spring.datasource.driver-class-name}")
+    private String driver;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Bean
+    public DataSource createDataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        dataSource.setAutoCommitOnClose(false);
+        dataSource.setInitialPoolSize(10);
+        dataSource.setMinPoolSize(10);
+        dataSource.setMaxPoolSize(100);
+        System.out.print("========================================="+dataSource+"===========================================");
+        return dataSource;
+
+    }
 	
 	/**
      * 分页插件
